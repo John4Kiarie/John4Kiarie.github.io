c0618bce2487fa48f33ca36aabd3ce1244accf20
John Kiarie <140244650+John4Kiarie@users.noreply.github.com>
2026-01-31 15:08:38 +0300

    Delete assets directory

--- assets/js/colors.js
+++
@@ -1,3 +0,0 @@
-
-const colors = ['#00ffcc', '#00ff00', '#ff00ff', '#ffcc00', '#0099ff'];
-document.documentElement.style.setProperty('--accent-color', colors[Math.floor(Math.random() * colors.length)]);
